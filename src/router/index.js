f8814973a5b4ca07f5390b8d45d88a1d1cdc2943
yunxics <yunxics@163.com>
2019-11-29 16:40:20 +0800

    vue-video-play

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import Home from '@/components/home';
+import camera from '@/components/camera.vue';
 
 Vue.use(Router)
 
@@ -14,8 +15,8 @@ export default new Router({
     },
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      name: 'camera',
+      component: camera
     }
   ]
 })
